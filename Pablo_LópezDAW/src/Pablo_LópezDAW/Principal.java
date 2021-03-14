24bf08a9a755ca14d4c6d22170bc13fde3052e0f
Pablo <plopezg18@iesalbarregas.es>
2021-03-14 16:03:40 +0100

    Linea añadidas

--- Pablo_LópezDAW/src/Pablo_LópezDAW/Principal.java
+++ Pablo_LópezDAW/src/Pablo_LópezDAW/Principal.java
@@ -1,5 +1,10 @@
 package Pablo_LópezDAW;
 
 public class Principal {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("primera linea");
+		System.out.println("segunda linea");
+		
+		}
 }
